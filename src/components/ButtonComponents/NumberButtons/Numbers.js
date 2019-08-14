6f55ec9767cf8bfda08c7a0449b1f0171340015e
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-13 19:47:26 -0700

    more coding completed

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -8,16 +8,17 @@ import {numbers} from "../../../data";
 
 const Numbers = (props) => {
   // STEP 2 - add the imported data to state
-  const [numberState] = useState(numbers);
+  const [buttonNumbers, setButtonNumbers] = useState(numbers);
   return (
     <div className="numberButtons">
       /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/
        
-       {numberState.map((item, index) => { 
+       {buttonNumbers.map((item, index) => { 
          return <NumberButton key={index} text={item}/>;
        })}
+       
     </div>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -8,12 +8,15 @@ import {specials} from "../../../data";
 
 const Specials = (props) => {
   // STEP 2 - add the imported data to state
-const [specialState] = useState(specials);
+const [specialButton] = useState(specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+           {specialButton.map((item, index) => { 
+         return <SpecialButton key={index} text={item}/>;
+       })}
     </div>
   );
 };
